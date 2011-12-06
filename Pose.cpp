269a01d65b617faa95752a5e776239a0b7301d69
DinoMan <dinovgk@gmail.com>
2011-12-06 18:21:35 +0000

    Might work

--- Pose.cpp
+++ Pose.cpp
@@ -1,16 +1,40 @@
-class Pose{
-    int x,y,theta;
-    public:
-        updatePose(){
-        distance=velocity*time
-        x+=distance*cos(theta);
-        y+=distance*sin(theta);
-        }
-        getPose(){}
+#include <sys/time.h>
 
-    Pose::Pose()
+typedef{
+double x,y,theta;
+}pose
+
+class Pose{
+    pose robotPose;
+    timeval startTime,endtime;
+    double velocity,distance;
+
+    public:
+        getTimestamp(){
+        gettimeofday(&starttime,NULL);
+        }
+        updatePosition(double distance){
+        gettimeofday(&endtime,NULL);
+        distance=velocity*((endtime.tv_sec * 1000000) + (endtime.tv_usec)) - ((starttime.tv_sec * 1000000) + (starttime.tv_usec));
+        robotPose.x+=distance*cos(robotPose.theta);
+        robotPose.y+=distance*sin(robotPose.theta);
+        }
+        updateAngle(double angle){
+        robotPose.theta=angle;
+        }
+        getPose(){
+        return robotPose;
+        }
+        setPose(double x,double y, double theta ){
+        robotPose.x=x;
+        robotPose.y=y;
+        robotPose.theta=z;
+        }
+    Pose::Pose(double x,double y, double theta )
     {
-        //ctor
+        robotPose.x=x;
+        robotPose.y=y;
+        robotPose.theta=z;
     }
 
     Pose::~Pose()
