09045fadc3b9aa53d48af6a83ddf576c639a5d57
DinoMan <dinovgk@gmail.com>
2011-12-14 22:06:18 +0000

    comments

--- Pose.cpp
+++ Pose.cpp
@@ -10,11 +10,11 @@ double ang_velocity;
 
 Pose::Pose(double x,double y, double theta)
 {
-    robotPose.x=x;
-    robotPose.y=y;
-    robotPose.theta=theta;
-    velocity = 15.5;
-    ang_velocity = 0.8253;
+    robotPose.x=x;//x coordinate of robot
+    robotPose.y=y;//y coordinate of robot
+    robotPose.theta=theta;//bearing of the robot
+    velocity = 15.5;//linear velocity of the robot according to least squares estimation
+    ang_velocity = 0.8253;//average angular velocity of the robot according to least squares estimation
 }
 
 Pose::~Pose()
@@ -32,31 +32,29 @@ void Pose::setTimestamp()
 
 void Pose::updateOdometry()
 {
-    gettimeofday(&endtime,NULL);
+    gettimeofday(&endtime,NULL);//get a timestamp that corresponds to the end of the previous action.
+    //calculate distance travelled based on linear velocity and the duration of the previous movement
     distance=velocity*(((endtime.tv_sec * 1000000) + (endtime.tv_usec)) - ((starttime.tv_sec * 1000000) + (starttime.tv_usec)))/1000000.0;
+    //depending on previous movement update odometry.
     if (MOVING_FORWARD==g_prev_movement){
     robotPose.x+=distance*cos(robotPose.theta);
     robotPose.y+=distance*sin(robotPose.theta);
-   // cout<<"I was moving fwd "<<"distance:"<<distance<< "prev mov: "<<g_prev_movement<< "cosine: "<<cos(robotPose.theta)<<endl;
     }
     else if(MOVING_BACKWARD==g_prev_movement){
     robotPose.x+=distance*cos(robotPose.theta+M_PI);
     robotPose.y+=distance*sin(robotPose.theta+M_PI);
-    //cout<<"I was moving Bckwd "<<"distance:"<<distance<< "prev mov: "<<g_prev_movement<< "cosine: "<<cos(robotPose.theta)<<endl;
     }
     else if (TURNING_LEFT==g_prev_movement){
         robotPose.theta+=ang_velocity*(((endtime.tv_sec * 1000000) + (endtime.tv_usec)) - ((starttime.tv_sec * 1000000) + (starttime.tv_usec)))/1000000.0;
         robotPose.theta=normRad(robotPose.theta);
-   // cout<<"I was moving left "<<"distance:"<<distance<<endl;
-    }
+      }
     else if(TURNING_RIGHT==g_prev_movement){
         robotPose.theta-=ang_velocity*(((endtime.tv_sec * 1000000) + (endtime.tv_usec)) - ((starttime.tv_sec * 1000000) + (starttime.tv_usec)))/1000000.0;
         robotPose.theta=normRad(robotPose.theta);
-    //cout<<"I was moving right "<<"distance:"<<distance<<endl;
     }
-    else if(STOPPED==g_prev_movement){ 
-     //cout<<"I was stopped "<<"distance: "<<distance<<endl;
+    else if(STOPPED==g_prev_movement){
     }
+    /*set the start time of the new movement and set the previous movement to current movement for the next call of the function*/
     g_prev_movement=g_movement;
     starttime.tv_sec=endtime.tv_sec;
     starttime.tv_usec=endtime.tv_usec;
@@ -66,8 +64,8 @@ void Pose::updateOdometry()
 
 DirTime Pose::shiftToGoal(SITE id_site)
 {
-    //get goal coordinates
-    
+    //get coordinates of the next goal
+
     goal_y=0;
     goal_x =0;
 
@@ -76,39 +74,39 @@ DirTime Pose::shiftToGoal(SITE id_site)
     goal_x=sites[id_site].readyPoint.x;
 
 
-    //relative position of goal to robot
-    //find angle
+    //find the angle of the line that connects the estimated robot position and the target point of next site.
     angle=atan2(goal_y-robotPose.y,goal_x-robotPose.x);
-    
+    /*
     cout<<"goal x: "<<goal_x<<endl;
     cout<<"goal y: "<<goal_y<<endl;
-    
     cout<<"Pose x: "<<robotPose.x<<endl;
-    
-     cout<<"Pose y: "<<robotPose.y<<endl;
-    
-    cout<<"After arctan"<<angle<<endl;
-    angle-=robotPose.theta;
+    cout<<"Pose y: "<<robotPose.y<<endl;
+    */
 
-    cout<<"Angle: "<<angle<<endl;
-    angle=normRad(angle);
-    cout<<"NormRad: "<<angle<<endl;
+    //cout<<"After arctan"<<angle<<endl;
+    angle-=robotPose.theta;//subtract the current bearing of the robot from the angle to find how much to turn
 
-    ret_val.time=abs((angle/ang_velocity)*1000);
+    //cout<<"Angle: "<<angle<<endl;
+    angle=normRad(angle);//normalize so that the angle we find is between minus pi and pi
+    //cout<<"NormRad: "<<angle<<endl;
+
+    ret_val.time=abs((angle/ang_velocity)*1000);//calculate time for turn based on turning angle
+    //decide on direction of turn depending on the turning angle sign
     if (angle<0){
         ret_val.direction=TURNING_RIGHT;
     }
     else{
         ret_val.direction=TURNING_LEFT;
     }
-return ret_val;
+return ret_val;//return the direction and time of the turn
 
 }
+//returns the pose of the robot in a structure
 RobotPose Pose::getPose()
 {
     return robotPose;
 }
-
+//allows the robot's pose to be set maually whenever a certain position is assumed
 void Pose::setPose(double x,double y, double theta )
 {
     robotPose.x=x;
@@ -117,7 +115,7 @@ void Pose::setPose(double x,double y, double theta )
 
     cout<<"New Position: (x,y, theta): "<<"("<<robotPose.x<<", "<<robotPose.y<<","<<robotPose.theta<<")"<<endl;
 }
-
+//calculates the distance to the next goal in cm
 int Pose::distanceToResourceSite(SITE site){
 
         double x_site = sites[site].readyPoint.x;
@@ -128,10 +126,11 @@ int Pose::distanceToResourceSite(SITE site){
         cout<<"Distance to site: "<<distanceToSite<<endl;
         return distanceToTime(distanceToSite);
 }
-
+//convertion of distance to time based on linear velocity.
 double Pose::distanceToTime(double distance){
 
-    double time = distance/velocity*1000;
+    double time = distance/velocity*1000;//convertion from cm to milisecs
+
 
     return time;
 
